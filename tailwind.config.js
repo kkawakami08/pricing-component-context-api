53fa77a9385dbd39893a58af086cf68619ee5bbe
Kaori Kawakami <k.kawakami08@gmail.com>
2023-05-05 18:55:43 -0700

    initial commit with project color palette and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        veryLightGrayishBlue: "hsl(240,78%,98%)",
+        lightGrayishBlue: "hsl(234,14%,74%)",
+        grayishBlue: "hsl(233,13%,49%)",
+        darkGrayishBlue: "hsl(232,13%,33%)",
+        gradientStart: "hsl(236,72%,79%)",
+        gradientEnd: "hsl(237,63%,64%)",
       },
     },
   },
   plugins: [],
-}
+};
